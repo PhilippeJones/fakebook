6b9b9f641359e1f946f681ae0ceaaa211c9f430e
PhilippeJones <philippejones@gmail.com>
2016-06-19 19:52:55 -0400

    Got profile and avatar editing in. Started a bit of AJAX stuff.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,6 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
-	# Only allow letter, number, underscore and punctuation.
-#	validates_format_of :name, with: /^[a-zA-Z0-9_\.]*$/, :multiline => false
-
-	has_attached_file :avatar, styles: { large: "1280x1280>", medium: "640x640#", small: "120x120#" }
+	has_attached_file :avatar, styles: { large: "1280x1280>", medium: "640x640#", small: "120x120#" }, default_url: "/images/avatar-missing-:style.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
